8ace7f0be88d1abbae9b03047fd31de64b788d06
GR-Menon <menon.gautam.013@gmail.com>
2024-06-02 09:20:26 +0530

    RESIDE dataset class

--- dataset/reside.py
+++ dataset/reside.py
@@ -1,73 +1,73 @@
 import os
-import numpy as np
 from PIL import Image
+import matplotlib.pyplot as plt
 
-from torch.utils.data import Dataset
-from torchvision.transforms import transforms
+import torch
+from torchvision import transforms as T
+from torch.utils.data import Dataset, DataLoader
 
 
 class ResideDataset(Dataset):
-    def __init__(self, root, img_size, train: bool = True, format='.png', transform: transforms.Compose = None):
+    def __init__(self, root: str, img_transform: T.Compose = None):
         super(ResideDataset, self).__init__()
 
-        self.img_size = img_size
-        self.train = train
-        self.format = format
-        self.transform = transform
-        self.haze_imgs = [os.path.join(root, 'hazy', img) for img in os.listdir(os.path.join(root, 'hazy'))]
+        self.root = root
+        self.haze_dir = os.path.join(root, 'hazy')
         self.clear_dir = os.path.join(root, 'clear')
+        self.haze_imgs = [os.path.join(root, 'hazy', img) for img in os.listdir(self.haze_dir)]
+        self.img_transform = img_transform
 
     def __len__(self):
         return len(self.haze_imgs)
 
-    def __getitem__(self, index):
-        haze = np.array(Image.open(self.haze_imgs[index]))
-        #
-        # if isinstance(self.img_size, int):
-        #     while haze.size[0] < self.img_size or haze.size[1] < self.img_size:
-        #         index = random.randint(0, 20000)
-        #         haze = Image.open(self.haze_imgs[index])
+    def __getitem__(self, idx) -> (torch.Tensor, torch.Tensor):
+        haze = Image.open(self.haze_imgs[idx])
 
-        img = self.haze_imgs[index]
-        id = img.split('/')[-1].split('_')[0]
+        clear_id = self.haze_imgs[idx].split('/')[-1].split('_')[0]
+        clear = Image.open(os.path.join(self.clear_dir, clear_id + ".png"))
 
-        clear_name = id + self.format
-        clear = np.array(Image.open(os.path.join(self.clear_dir, clear_name)))
-        # clear = transforms.CenterCrop(haze.size[::-1])(clear)
-        #
-        # if not isinstance(self.img_size, str):
-        #     i, j, h, w = transforms.RandomCrop.get_params(haze, output_size=(self.img_size, self.img_size))
-        #     haze = TF.crop(haze, i, j, h, w)
-        #     clear = TF.crop(clear, i, j, h, w)
-        if self.transform:
-            haze, clear = self.transform(haze.convert('RGB'), clear.convert('RGB'))
+        haze, clear = haze.convert('RGB'), clear.convert('RGB')
+
+        if self.img_transform:
+            haze, clear = self.img_transform(haze), self.img_transform(clear)
         return haze, clear
-
-    # def augmentations(self, data, target):
-    #     if self.train:
-    #         rand_hor = random.randint(0, 1)
-    #         rand_rot = random.randint(0, 3)
-    #         data = transforms.RandomHorizontalFlip(rand_hor)(data)
-    #         target = transforms.RandomHorizontalFlip(rand_hor)(target)
-    #         if rand_rot:
-    #             data = TF.rotate(data, 90 * rand_rot)
-    #             target = TF.rotate(target, 90 * rand_rot)
-    #     data = transforms.ToTensor()(data)
-    #     data = transforms.Normalize(mean=[0.64, 0.6, 0.58], std=[0.14, 0.15, 0.152])(data)
-    #     target = transforms.ToTensor()(target)
-    #     return data, target
 
 
 def test_reside():
     train_path = "../RESIDE-Std"
     test_path = "../RESIDE-Synth/indoor"
 
-    train_dataset = ResideDataset(train_path, train=True, img_size=240)
-    test_dataset = ResideDataset(test_path, train=False, img_size='whole_img')
+    train_transforms = T.Compose([
+        T.Resize((256, 256)),
+        T.RandomHorizontalFlip(p=0.5),
+        T.ToTensor(),
+        T.Normalize(mean=[0.5] * 3, std=[0.5] * 3),
+    ])
+    test_transforms = T.Compose([
+        T.Resize((256, 256)),
+        T.ToTensor(),
+        T.Normalize(mean=[0.5] * 3, std=[0.5] * 3),
+    ])
+
+    train_dataset = ResideDataset(train_path, img_transform=train_transforms)
+    test_dataset = ResideDataset(test_path, img_transform=test_transforms)
 
     print(len(train_dataset), len(test_dataset))
-    img, target = train_dataset[0]
-    print(img.shape, target.shape)
+    train_img, train_target = train_dataset[0]
+    test_img, test_target = test_dataset[0]
+
+    plots = [train_img, train_target, test_img, test_target]
+    plt.figure(figsize=(8, 8))
+    for i in range(4):
+        plt.subplot(2, 2, i + 1)
+        plt.axis(False)
+        plt.title(plots[i].shape)
+        plt.imshow(plots[i].permute(1, 2, 0))
+    plt.show()
+
+    train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+    img1, img2 = next(iter(train_loader))
+    print(img1.shape, img2.shape)
 
 
 if __name__ == '__main__':
